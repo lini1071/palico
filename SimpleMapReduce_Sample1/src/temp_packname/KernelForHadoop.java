88f4f32a98d28a533b9afe1548b528827fb6afbe
lini1071 <riny1071@gmail.com>
2016-04-04 21:19:56 +0900

    Uploaded SimpleProgram for Hadoop MapReduce

---
+++ SimpleMapReduce_Sample1/src/temp_packname/KernelForHadoop.java
@@ -0,0 +1,20 @@
+package temp_packname;
+
+// this class was not implemented yet.
+class KernelForHadoop extends com.amd.aparapi.Kernel
+{
+	// Constructor
+	public KernelForHadoop()
+	{
+		
+		
+	};
+	
+	// Kernel must override public void run() method.
+	@Override
+	public void run()
+	{
+		
+		
+	};
+}
