a971e7b540ad388bf80f9ff5cf8f2768233a23a4
lini1071 <riny1071@gmail.com>
2016-04-07 13:48:31 +0900

    CalculatorMapper.java 수정. 의도한 대로 동작함을 확인. 현재 코드에서는 값을 곱하도록 작성되어 있음.

--- src/org/ground/palico/hadoop/CalculatorMapper.java
+++ src/org/ground/palico/hadoop/CalculatorMapper.java
@@ -9,17 +9,17 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class CalculatorMapper extends
 	Mapper<LongWritable, FloatWritable, LongWritable, FloatWritable> {
 	
-    private static final float SAMPLE_VALUE = 2.3f;
-    private FloatWritable outValue = new FloatWritable();
-
-    // map function implementing
-    @Override
-    public void map(LongWritable key, FloatWritable value, Context context) throws IOException, InterruptedException {
-
-        // process calculating
-        outValue.set(value.get() * SAMPLE_VALUE);
-
-        // write key-value pair to the split
-        context.write(key, outValue);
-    }
+	private static final float SAMPLE_VALUE = 2.3f;
+	private FloatWritable outValue = new FloatWritable();
+	    
+	// map function implementing
+	@Override
+	public void map(LongWritable key, FloatWritable value, Context context) throws IOException, InterruptedException {
+	
+		// process calculating
+		outValue.set(value.get() * SAMPLE_VALUE);
+		// write key-value pair to the split
+		context.write(key, outValue);
+	
+	}
 }
