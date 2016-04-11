4633a17306b8802901ca6d6e529414437b369d02
이태경 <tklee@satreci.com>
2016-04-11 16:13:22 +0900

    리팩토링 ExecutionMode, ExecutionResult, ComplexityBenchmarkInput 소개

--- src/org/ground/palico/gpu/aparapi/AparapiBandOperator.java
+++ src/org/ground/palico/gpu/aparapi/AparapiBandOperator.java
@@ -3,20 +3,24 @@ package org.ground.palico.gpu.aparapi;
 import com.amd.aparapi.Kernel;
 import com.amd.aparapi.Range;
 import com.amd.aparapi.device.Device;
+import org.ground.palico.ComplexityBenchmarkInput;
 
 public abstract class AparapiBandOperator {
     /**
      * 밴드 연산 실행 부분
      * 템플릿 메소드
      */
-    public void run(int size, float[] band1, float[] band2, float[] band3, float[] result, Kernel.EXECUTION_MODE mode){
-        Kernel kernel = getKernel(band1, band2, band3, result);
+    public float[] run(ComplexityBenchmarkInput input, Kernel.EXECUTION_MODE mode){
+        final int size = input.getDataSize();
+        final float[] result = new float[size];
+        Kernel kernel = getKernel(input.getBand1(), input.getBand2(), input.getBand3(), result);
         Device device = Device.best();
         Range range = device.createRange(size);
 
         kernel.setExecutionMode(mode);
         kernel.execute(range);
         kernel.dispose();
+        return result;
     }
 
     protected abstract Kernel getKernel(float[] band1, float[] band2, float[] band3, float[] result);
